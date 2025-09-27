0504cfb657a795ab5a9244b020669fa379f45daa
Joao Bastos <joaobastos@outlook.de>
2025-09-27 21:34:13 +0200

    css: svg, bento

--- script/scroll.js
+++ script/scroll.js
@@ -3,5 +3,5 @@ scrollAnimate({
   heightStart: 0.95,
   heightEnd: 0.3,
   from: { opacity: 0, scale: 0.95, transformY: 50, borderRadius: 100},
-  to:   { opacity: 1, scale: 1,   transformY: 0,  borderRadius: 32},
+  to:   { opacity: 1, scale: 1,   transformY: 0,  borderRadius: 52},
 });
